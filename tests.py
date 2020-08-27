1e4026813ddde176c8b4f940dbf5ee88b0807cb6
AlejoG10 <alejogonza99@hotmail.com>
2020-08-27 11:30:23 -0500

    New test setup has been added

--- tests.py
+++ tests.py
@@ -5,17 +5,19 @@ class Test(unittest.TestCase):
 
 	def setUp(self):
 		self.setup1 = [ 5, 3, 0, 4, 1, 2 ]
+		self.setup2 = [ -10, 5, 15, -5, -15, 10 ]
 
 	def tearDown(self):
 		del( self.setup1 )
+		del( self.setup2 )
 
 	def test_bubble(self):
 		bubble_sort( self.setup1 )
 		self.assertTrue( self.setup1 == [ 0, 1, 2, 3, 4, 5 ] )
 
 	def test_selection(self):
-		selection_sort( self.setup1 )
-		self.assertTrue( self.setup1 == [ 0, 1, 2, 3, 4, 5 ] )
+		selection_sort( self.setup2 )
+		self.assertTrue( self.setup2 == [ -15, -10, -5, 5, 10, 15 ] )
 
 if __name__ == '__main__':
 	unittest.main()
